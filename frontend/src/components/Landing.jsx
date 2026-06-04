15f9cf74db3a565d521b617d6e8db4c84b67d85f
Omarf8 <omarfigueroa223@gmail.com>
2026-06-03 19:48:45 -0500

    landing page basics

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,5 +1,22 @@
 function Landing() {
+	const authLogin = async () => {
+		const response = await fetch("http://localhost:8000/auth/login/")	
+		const data = await response.json()
 
+		window.location.href = data.url
+	}
+
+	return (
+		<div>
+			<div>
+				<h1>CourseMap</h1>
+				<h4>Quickly Extract Dates From Syllabi and Populate Your Calendar!</h4>
+			</div>
+			<div>
+				<button onClick={authLogin}>Sign in with Google</button>
+			</div>
+		</div>
+	)
 }
 
 export default Landing
