234430de0b6d1c8078f2bb8247d1848bf9218ff2
Omarf8 <omarfigueroa223@gmail.com>
2026-06-13 19:20:39 -0500

    update landing page with coursemap logo and revamped

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,5 +1,6 @@
 import styles from './Landing.module.css'
 import { FcGoogle } from 'react-icons/fc'
+import coursemapLogo from '../assets/coursemap.png'
 
 function Landing() {
 	const authLogin = async () => {
@@ -11,12 +12,17 @@ function Landing() {
 
 	return (
 		<div className={styles.content}>
-			<div>
-				<h1>CourseMap</h1>
-				<h4>Quickly Extract Dates From Syllabi and Populate Your Calendar!</h4>
-			</div>
-			<div>
-				<button className={styles.google}onClick={authLogin}><FcGoogle size={20} /> Sign in with Google</button>
+			<div className={styles["inner-content"]}>
+				<div> 
+					<div className={styles.brand}>
+						<img src={coursemapLogo} alt=""/>
+						<h1>CourseMap</h1>
+					</div>
+					<h4>Quickly Extract Dates From Syllabi and Populate Your Calendar!</h4>
+				</div>
+				<div>
+					<button className={styles.google}onClick={authLogin}><FcGoogle size={20} /> Sign in with Google</button>
+				</div>
 			</div>
 		</div>
 	)
